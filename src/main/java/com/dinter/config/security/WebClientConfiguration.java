ffe85973792a8efc247eb0fd7e7c8e4773dfae7c
markchampion <langtuyetvuanh.1999@gmail.com>
2024-08-06 18:02:46 +0700

    config default registration id

--- src/main/java/com/dinter/config/security/WebClientConfiguration.java
+++ src/main/java/com/dinter/config/security/WebClientConfiguration.java
@@ -1,5 +1,6 @@
 package com.dinter.config.security;
 
+import com.dinter.config.data.OAuth2ClientProperties;
 import lombok.val;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,9 +11,16 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfiguration {
 
+    private final OAuth2ClientProperties properties;
+
+    public WebClientConfiguration(OAuth2ClientProperties properties) {
+        this.properties = properties;
+    }
+
     @Bean
     public WebClient webClient(ReactiveOAuth2AuthorizedClientManager reactiveOAuth2AuthorizedClientManager) {
         val serverOAuth2AuthorizedClientExchangeFilterFunction = new ServerOAuth2AuthorizedClientExchangeFilterFunction(reactiveOAuth2AuthorizedClientManager);
+        serverOAuth2AuthorizedClientExchangeFilterFunction.setDefaultClientRegistrationId("github");
         return WebClient.builder()
                 .filter(serverOAuth2AuthorizedClientExchangeFilterFunction)
                 .build();
