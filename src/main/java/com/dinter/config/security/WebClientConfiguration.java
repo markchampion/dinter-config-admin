f788e3539e24bae3a88535b1be710633544d1d09
markchampion <langtuyetvuanh.1999@gmail.com>
2024-08-06 13:27:41 +0700

    optimize import, add @Configuration

--- src/main/java/com/dinter/config/security/WebClientConfiguration.java
+++ src/main/java/com/dinter/config/security/WebClientConfiguration.java
@@ -1,9 +1,11 @@
 package com.dinter.config.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@Configuration
 public class WebClientConfiguration {
 
     @Bean
