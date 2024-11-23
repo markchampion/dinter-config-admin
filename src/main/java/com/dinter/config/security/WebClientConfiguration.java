75a3a59c741e133391835bada839fc6372323967
markchampion <langtuyetvuanh.1999@gmail.com>
2024-11-23 19:12:35 +0700

    config default webclient when there is no oauth2 client config

--- src/main/java/com/dinter/config/security/WebClientConfiguration.java
+++ src/main/java/com/dinter/config/security/WebClientConfiguration.java
@@ -1,6 +1,7 @@
 package com.dinter.config.security;
 
 import lombok.val;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
@@ -11,12 +12,19 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfiguration {
 
     @Bean
-    public WebClient webClient(ReactiveOAuth2AuthorizedClientManager reactiveOAuth2AuthorizedClientManager) {
+    @ConditionalOnProperty(name = "spring.security.oauth2.client")
+    public WebClient keyCloakWebClient(ReactiveOAuth2AuthorizedClientManager reactiveOAuth2AuthorizedClientManager) {
         val serverOAuth2AuthorizedClientExchangeFilterFunction = new ServerOAuth2AuthorizedClientExchangeFilterFunction(reactiveOAuth2AuthorizedClientManager);
         serverOAuth2AuthorizedClientExchangeFilterFunction.setDefaultClientRegistrationId("keycloak");
 
         return WebClient.builder()
                 .filter(serverOAuth2AuthorizedClientExchangeFilterFunction)
+                .build();
+    }
+    @Bean
+    @ConditionalOnProperty(name = "spring.security.oauth2.client", havingValue = "true", matchIfMissing = true)
+    public WebClient webClient() {
+        return WebClient.builder()
                 .build();
     }
 }
